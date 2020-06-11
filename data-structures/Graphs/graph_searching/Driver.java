08acb825e546efc3932c32b59aa8722019e679c3
Nicholas O'Kelley <leadslinger32@gmail.com>
2020-06-11 01:40:11 -0400

    Had to revert because i deleted all java files. oop

--- data-structures/Graphs/graph_searching/Driver.java
+++ data-structures/Graphs/graph_searching/Driver.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * The driver for my graph program. 
  *
@@ -6,12 +12,47 @@
  */
 public class Driver {
 
-    public static void main(String[]){
-        if(args.length < 2 || args.length > 2){
+    public static void main(String[] args){
+        if(args.length < 1){
             System.out.println("Usage: <mapName> <search>");
-            Sytem.exit(0);
+            System.exit(0);
         }
+        
+        Scanner scan = new Scanner(System.in);
         Graph g = new Graph();
         g.buildGraph(args[0]);
+        boolean running = true;
+
+        while(running){
+            try{
+                Scanner source = new Scanner(System.in);
+                System.out.println("Enter the source vertex #: ");
+                int sourceVertex = source.nextInt();
+                while(sourceVertex < 0){
+                    System.out.println("Can't you read? Try again.");
+                    System.out.println("Enter the source vertex #: ");
+                    sourceVertex = source.nextInt();
+                }
+
+            } catch(InputMismatchException e){
+                System.out.println("Can't you read? Integers are accepted here!");
+            }
+            try{
+                Scanner dest = new Scanner(System.in);
+                System.out.println("Enter the source vertex #: ");
+                int destVertex = dest.nextInt();
+                while(destVertex < 0){
+                    System.out.println("Can't you read? Try again.");
+                    System.out.println("Enter the source vertex #: ");
+                    destVertex = dest.nextInt();
+                }
+
+            } catch(InputMismatchException e){
+                System.out.println("Can't you read? Integers are accepted here!");
+            }
+
+            g.
+
+        }
     }
 }
