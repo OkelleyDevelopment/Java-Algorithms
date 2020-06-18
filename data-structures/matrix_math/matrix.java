b7d416cd7d1800fe9ff8c3ceccd31ae6b3ae9b50
Nicholas O'Kelley <leadslinger32@gmail.com>
2020-06-17 23:56:14 -0400

    Added the matrix_math directory

---
+++ data-structures/matrix_math/matrix.java
@@ -0,0 +1,14 @@
+/**
+ * Hopefully, a simple implementation of a matrix in java
+ * that uses transitive closure.
+ *
+ * @author Nicholas O'Kelley
+ *
+ * @version June 17, 2020
+ */
+public class matrix{
+
+    public matrix(){
+
+    }
+}
